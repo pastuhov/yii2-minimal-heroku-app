6b218c3ae1c68ddaa0c1a0a7364f90362e4cce7e
Kirill Pastukhov <kirill@pastukhov.su>
2015-11-05 22:51:58 +0500

    add dotenv

--- config/web.php
+++ config/web.php
@@ -12,7 +12,7 @@ $config = [
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'YZcbdjZbSECsL31_uRE8GgCY9POqNlgO',
+            'cookieValidationKey' => getenv('cookieValidationKey'),
         ],
         /*
         'urlManager' => [
--- web/index.php
+++ web/index.php
@@ -1,10 +1,14 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require(__DIR__ . '/../vendor/autoload.php');
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
+
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
