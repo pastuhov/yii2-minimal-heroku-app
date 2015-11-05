a61f826b9a4e188745c3989e1088b1982274c80e
Kirill Pastukhov <kirill@pastukhov.su>
2015-11-06 01:02:06 +0500

    Set enablePrettyUrl=true

--- config/web.php
+++ config/web.php
@@ -14,14 +14,13 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => getenv('COOKIE_VALIDATION_KEY'),
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
